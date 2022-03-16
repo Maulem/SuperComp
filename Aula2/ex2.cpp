efbacd494907a097a4258a0354cc4862195b15c1
Maulem <henriquemmarti@gmail.com>
2022-03-16 17:19:41 -0300

    Ex6

--- Aula2/ex2.cpp
+++ Aula2/ex2.cpp
@@ -1,5 +1,17 @@
-#include <iostream>
+// g++ -Wall ex2.cpp -o ex2
+
+#include <iostream>     // std::cout, std::fixed
+#include <iomanip>      // std::setprecision
+#include <cmath>
 
 int main() {
-    return 1;
+    int n;
+    double s = 0.0;
+    std::cin >>n;
+    for (int i = 0; i <=n; i++) {
+         s += 1 / std::pow(2, i);
+    }
+    std::cout << std::fixed; // Faz sempre aparecer os valores até 15 casa>
+    std::cout << std::setprecision(15) << s << '\n';
+    return 0;
 }
---
+++ Aula2/ex6.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <iomanip>      // std::setprecision
+
+int main() {
+    int n;
+    std::cout << "Numero de pontos: ";
+    std::cin >> n;
+    std::cout << "\n";
+
+    std::vector<std::vector<double>> matriz;
+    for (int i = 0; i < n; i++) {
+        std::vector<double> linha;
+        double x, y;
+
+        std::cout << "X: ";
+        std::cin >> x;
+        std::cout << "\n";
+
+        std::cout << "Y: ";
+        std::cin >> y;
+        std::cout << "\n";
+
+        linha.push_back(x);
+        linha.push_back(y);
+        matriz.push_back(linha);
+    }
+
+    std::cout << "Matriz entrada: " << "\n";
+
+    for (int i = 0; i < n; i++) {
+        std::cout << std::setprecision(2) << std::fixed << matriz[i][0] << "  ";
+        std::cout << std::setprecision(2) << std::fixed << matriz[i][1] << "\n";
+    }
+
+    
+
+    // Calculo da distancia
+    std::vector<std::vector<double>> ansmatriz;
+
+    for (int i = 0; i < n; i++) {
+        std::vector<double> linha;
+        for (int j = 0; j < n; j++) {
+            double DX = matriz[j][0] - matriz[i][0];
+            double DY = matriz[j][1] - matriz[i][1];
+            linha.push_back(std::sqrt(DX*DX + DY*DY));
+        }
+        ansmatriz.push_back(linha);
+    }
+
+    std::cout << "Matriz saida: " << "\n";
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            std::cout << std::setprecision(2) << std::fixed << ansmatriz[i][j] << "  ";
+        }
+        std::cout << "\n";
+    }
+
+    
+    return 1;
+}
